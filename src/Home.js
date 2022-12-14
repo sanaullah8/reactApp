27ad6655e87f67d08cefa0184a1e5a1bca5c5b39
sanaullah8 <sanaullahsallu25@gmail.com>
2022-12-14 16:45:43 +0500

    added map function with usse state

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,31 @@
 import { useState } from "react";
 
 const Home = () => {
-  const [exe, exeName] = useState("hassan");
-  const [age, setAge] = useState(25);
-  const handleClick = () => {
-    exeName("Aftab");
-    setAge(36);
-  };
+  const [blogs, setBlogs] = useState([
+    {
+      title: "My new Website",
+      body: "lorem ipsum ...",
+      author: "mario",
+      id: 1,
+    },
+    { title: "Welcome party", body: "lorem ipsum ...", author: "yoshi", id: 2 },
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsum ...",
+      author: "yoko",
+      id: 3,
+    },
+  ]);
+
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <p>
-        {exe} is {age} old
-      </p>
-      <button onClick={handleClick}>Click Me</button>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2> {blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 };
-
 export default Home;
